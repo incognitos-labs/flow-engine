7ebaced5dc3c1ee50872b794fc712a7f308d0021
incognito <falomojohn@gmail.com>
2025-12-27 00:00:24 +0100

    add workflow dummy action

--- backend/workflow-exec.go
+++ backend/workflow-exec.go
@@ -8,10 +8,19 @@ import (
 func triggerHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Workflow triggered")
 	w.Write([]byte("OK"))
+	dummyAction()
+}
+
+func dummyAction() {
+	fmt.Println("Action executed")
 }
 
 func main() {
 	http.HandleFunc("/trigger", triggerHandler)
-	http.ListenAndServe(":8080", nil)
-	fmt.Println("Workflow Engine Server Running!")
+
+	fmt.Println("Workflow Engine Server Running on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
 }
